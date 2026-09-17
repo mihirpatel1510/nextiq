fc55edb373d5f035e2eec9317c9bac948a3435b4
mihirpatel1510 <mihir.patel@team.satat.tech>
2026-09-17 10:59:35 +0530

    fix(constants): update SERVICE_URL to development environment fix(pyproject): add frappe dependency for version management

--- nextiq/constants.py
+++ nextiq/constants.py
@@ -2,4 +2,4 @@
 # Copyright (c) 2026, krushang.patel@satat.tech and contributors
 # For license information, please see license.txt
 
-SERVICE_URL = "https://nextiq.in"
+SERVICE_URL = "https://niqa-dev.frappe.cloud"
